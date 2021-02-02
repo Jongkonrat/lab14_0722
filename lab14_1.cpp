37bb8efb506e6033e5996635d0c90b6d7354c2c1
Jongkonrat <jongkonrat_pisit@cmu.ac.th>
2021-02-02 15:08:25 +0700

    completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -18,7 +18,7 @@ int main(){
 	*x = 6;
 	cout << a << " " << b << " " << c << " " << &x << " " << (void *)y << " " << &z << endl;
 	*z = 7;
-	cout << a << " " << b << " " << c << " " << &x << " " << (void *)y << " " << &z << endl;
+	cout << a << " " << b << " " << c << " " << &x << " " << (void *)y << " " << &z;
 	
 	return 0;
 }
